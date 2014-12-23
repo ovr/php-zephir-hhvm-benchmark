959cb22e90ea7d515498716ae959d743d00dba69
Дмитрий Пацура <talk@dmtry.me>
2014-12-23 22:35:19 +0900

    Write benchmark

---
+++ benchmark.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * @author Patsura Dmitry <talk@dmtry.me>
+ */
+
+class Benchmark {
+    public function math($count)
+    {
+        for ($i=0; $i < $count; $i++) {
+            abs($i);
+        }
+
+        for ($i=0; $i < $count; $i++) {
+            acos($i);
+        }
+
+        for ($i=0; $i < $count; $i++) {
+            asin($i);
+        }
+
+        for ($i=0; $i < $count; $i++) {
+            atan($i);
+        }
+
+        for ($i=0; $i < $count; $i++) {
+            bindec($i);
+        }
+
+        for ($i=0; $i < $count; $i++) {
+            floor($i);
+        }
+
+        for ($i=0; $i < $count; $i++) {
+            exp($i);
+        }
+    }
+
+    public static function bench()
+    {
+        $count = 14000000;
+
+        $class = new Benchmark();
+        $class->math($count);
+    }
+}
