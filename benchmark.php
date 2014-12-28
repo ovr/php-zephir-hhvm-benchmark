093caf31434f146a46cc81574c3c5332ff54455b
Дмитрий Пацура <talk@dmtry.me>
2014-12-28 14:58:49 +0900

    Print table

--- benchmark.php
+++ benchmark.php
@@ -3,6 +3,60 @@
  * @author Patsura Dmitry <talk@dmtry.me>
  */
 
+namespace Bench;
+
+class Table
+{
+    protected $headers;
+
+    protected $rows;
+
+    protected $maxWidth = 8;
+
+    public function setHeaders(array $headers)
+    {
+        $this->headers = $headers;
+
+        foreach ($headers as $header) {
+            $this->maxWidth = max(strlen($header), $this->maxWidth);
+        }
+    }
+
+    public function addRow(array $row)
+    {
+        $this->rows[] = $row;
+
+        foreach ($row as $header) {
+            $this->maxWidth = max(strlen($header), $this->maxWidth);
+        }
+    }
+
+    public function printTable()
+    {
+        echo str_repeat('-', (($this->maxWidth+3)*count($this->headers))+1) . PHP_EOL;
+
+        echo '|';
+
+        foreach ($this->headers as $header) {
+            echo ' ' . $header . (str_repeat(' ', $this->maxWidth-strlen($header))) . ' |';
+        }
+
+        echo PHP_EOL;
+        echo str_repeat('-', (($this->maxWidth+3)*count($this->headers))+1) . PHP_EOL;
+
+        foreach ($this->rows as $row) {
+            echo '|';
+
+            foreach ($row as $header) {
+                echo ' ' . $header . (str_repeat(' ', $this->maxWidth-strlen($header))) . ' |';
+            }
+
+            echo PHP_EOL;
+            echo str_repeat('-', (($this->maxWidth+3)*count($this->headers))+1) . PHP_EOL;
+        }
+    }
+}
+
 class Benchmark
 {
     const SIMPLE_BOOL = true;
@@ -94,12 +148,16 @@ class Benchmark
         }
     }
 
+    static protected $statistics;
+
     public static function benchCall($class, $method, $count)
     {
         $startTime = microtime(true);
         $class->{$method}($count);
 
         $elapsed = microtime(true) - $startTime;
+        self::$statistics[$method] = $elapsed;
+
         echo ucfirst($method) . ": ($elapsed) sec." . PHP_EOL;
 
         return $elapsed;
@@ -111,22 +169,25 @@ class Benchmark
         echo "OS: " . PHP_OS . PHP_EOL;
         echo "Info: " . php_uname('a') . PHP_EOL;
 
-        echo str_repeat('-', 40) . PHP_EOL;
+        echo str_repeat('=', 40) . PHP_EOL;
 
         $max = -10;
         $min = 10;
 
         $count = 1400000;
-        $rounds = 15;
+        $rounds = 1;
 
         $class = new Benchmark();
 
+        $allStatistics = array();
+
         for ($i = 0; $i < $rounds; $i++) {
+            self::$statistics = [];
             $total = 0;
 
             echo "Round " . ($i+1) . "...." . PHP_EOL;
             $total += Benchmark::benchCall($class, 'math', $count);
-            
+
             $total += Benchmark::benchCall($class, 'methodCallEmpty', $count);
             $total += Benchmark::benchCall($class, 'methodCallBool', $count);
 
@@ -141,8 +202,28 @@ class Benchmark
 
             $min = min($min, $total);
             $max = max($max, $total);
+
+            $allStatistics[] = self::$statistics;
         }
 
+        $table = new Table();
+        $table->setHeaders(array('Test', 'Min', 'Max', 'Avg'));
+
+        foreach ($allStatistics[0] as $key => $row) {
+            $minRow = 10;
+            $maxRow = -10;
+            $avgRow = 0;
+
+            for ($i = 0; $i < $rounds; $i++) {
+                $value = $allStatistics[$i][$key];
+
+                $minRow = min($minRow, $value);
+                $maxRow = min($maxRow, $value);
+                $avgRow += $value;
+            }
+
+            $table->addRow([$key, $minRow, $maxRow, $avgRow/$rounds]);
+        }
 
         /**
          * 0.1 - 60 000
@@ -152,7 +233,10 @@ class Benchmark
         $perfect = 0.1;
         $perfectScore = 60000;
 
-        echo str_repeat('-', 40) . PHP_EOL;
+        echo str_repeat('=', 40) . PHP_EOL;
+
+        $table->printTable();
+
         echo "Max: " . $max . PHP_EOL;
         echo "Min: " . $min . PHP_EOL;
 
